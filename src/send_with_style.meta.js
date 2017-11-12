a4a4c4ddfec291f49f502fadd6933fdad439f4b9
Doğan Çelik <dogancelik93@gmail.com>
2017-11-12 14:07:37 +0300

    Publish 4.2.0

--- src/send_with_style.meta.js
+++ src/send_with_style.meta.js
@@ -3,7 +3,7 @@
 // @namespace   dogancelik.com
 // @description Enables font styles in IRCCloud
 // @include     https://www.irccloud.com/*
-// @version     4.1.0
+// @version     4.2.0
 // @grant       none
 // @updateURL   https://github.com/dogancelik/irccloud-sws/raw/master/build/send_with_style.meta.js
 // @downloadURL https://github.com/dogancelik/irccloud-sws/raw/master/build/send_with_style.user.js
